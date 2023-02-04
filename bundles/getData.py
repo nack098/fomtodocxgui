a1e436ac8a13b37409c0eb0a8f7a317dd689ac0c
nack098 <nack098gamer@gmail.com>
2023-02-05 00:37:29 +0700

    Add Update Fucntion and Change data structure

--- bundles/getData.py
+++ bundles/getData.py
@@ -1,25 +1,36 @@
 import json
 import sys
-import gspread 
+import gspread
+import string
 
-def getData(cerdKey) :
+
+def getData(cerdKey):
     account = gspread.service_account(cerdKey)
     spreadsheetList = account.list_spreadsheet_files()
-    returnValue = [] 
-    for sheet in spreadsheetList :
+    returnValue = []
+    for sheet in spreadsheetList:
         worksheetList = account.open(sheet["name"]).worksheets()
-        for worksheetnum, worksheet in enumerate(worksheetList) :
+        for worksheetnum, worksheet in enumerate(worksheetList):
             dataList = account.open(sheet["name"]).worksheet(worksheet.title).get_all_records()
             dataList = [{str(key): str(value) for key,value in data.items()} for data in dataList]
-            for data in dataList :
-                data.update({"sheet": sheet["name"], "worksheet": worksheet.title})
+            for index, data in enumerate(dataList):
+                data.update(
+                    {"sheet": sheet["name"],
+                     "worksheet": worksheet.title}
+                )
+                if data.get("Status"):
+                    data.update(
+                        {"index": f'{string.ascii_uppercase[list(data.keys()).index("Status")]}{index+2}'}
+                    )
             returnValue += dataList
-    returnValue = json.dumps(returnValue) 
+    returnValue = json.dumps(returnValue)
     print(returnValue)
 
-def main() :
+
+def main():
     cerdKey = sys.argv[1]
     getData(cerdKey)
 
-if __name__ == "__main__" :
+
+if __name__ == "__main__":
     main()
